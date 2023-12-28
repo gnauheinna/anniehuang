ab0497e6e46e0fddbcb563ab26df20760753f35a
annie <huangtc@bu.edu>
2023-12-28 13:43:57 -0500

    finsihed setting up hero structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,19 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Annie</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            {" "}
+            I develop end-to-end multiplation applications,
+            <br className="sm:block hidden" /> build cyberattack demonstrations,
+            and websecurity analysis
+          </p>
+        </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };

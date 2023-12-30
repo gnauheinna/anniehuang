ed216baaa2d41d9dac9234fd6bd5f490ab9c4061
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-12-30 18:00:47 -0500

    finished overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-
+import { SectionWrapper } from "../hoc";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -55,4 +55,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");

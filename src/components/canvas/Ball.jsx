0ad3798a9f5b347d7341f1f1ca358e5c87861163
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-12-31 16:46:40 -0500

    updated icosahedrongeometry ball

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -12,11 +12,28 @@ import CanvasLoader from "../Loader";
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
-  return <div>Ball</div>;
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+      </mesh>
+    </Float>
+  );
 };
 
 const BallCanvas = ({ icon }) => {
-  return <Canvas></Canvas>;
+  return (
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
 };
 
-export default Ball;
+export default BallCanvas;
